de5e03dfc60c949ec8293130fb989a2c7ecc3418
Yunior <yramirezc@gmail.com>
2018-05-21 19:14:32 +0200

    Detail

--- create_file_coll_stanford_corenlp.py
+++ create_file_coll_stanford_corenlp.py
@@ -6,7 +6,7 @@ import os
 from handler_json_nvd import HandlerCVEJson
 
 handler = HandlerCVEJson(sys.argv[1], range(2013,2018))
-stanford_corenlp_outfolder_name = "stanford-corenlp-processing-nvd-5yr"   # For 2013-2017 experiment
+stanford_corenlp_outfolder_name = "stanford-corenlp-processing-nvd-2013-2017_10_31"   # For 2013-2017 experiment
 filelistfl = open(os.path.join(sys.argv[1], stanford_corenlp_outfolder_name, "filelist-2011.txt"), 'wt')
 
 for i in range(0, handler.countCVEDescr()):
